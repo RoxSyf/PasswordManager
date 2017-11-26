13194cd5373b0c4f95ce8d078fba45331181ddb4
Roxane Sancho y Fran <41077@etu.he2b.be>
2017-11-26 12:00:47 +0100

    Add database write/read test

---
+++ app/src/main/java/he2b/esi/g41077/passwordmanager/AddEntryActivity.java
@@ -0,0 +1,13 @@
+package he2b.esi.g41077.passwordmanager;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddEntryActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_entry);
+    }
+}
