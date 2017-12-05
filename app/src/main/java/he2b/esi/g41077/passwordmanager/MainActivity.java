9ea0df0ab5d13b837dbae4cd0b926b8d03d1b7ef
Roxane Sancho y Fran <41077@etu.he2b.be>
2017-12-05 16:58:38 +0100

    Refactor

--- app/src/main/java/he2b/esi/g41077/passwordmanager/MainActivity.java
+++ app/src/main/java/he2b/esi/g41077/passwordmanager/MainActivity.java
@@ -58,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
                 etName.setText(entry.getmName());
             }
         });
-        
+
         // firebase
         initFirebase();
         addEventFirebaseListener();
