997a07968ab379353a1e38fe4b03ff625aea9033
Roxane Sancho y Fran <41077@etu.he2b.be>
2017-12-10 22:05:12 +0100

    create test class

---
+++ app/src/test/java/he2b/esi/g41077/passwordmanager/controller/EntryListFragmentTest.java
@@ -0,0 +1,29 @@
+package he2b.esi.g41077.passwordmanager.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EntryListFragmentTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void onCreate() throws Exception {
+    }
+
+    @Test
+    public void onCreateView() throws Exception {
+    }
+
+    @Test
+    public void onCreateOptionsMenu() throws Exception {
+    }
+
+    @Test
+    public void onOptionsItemSelected() throws Exception {
+    }
+
+}
